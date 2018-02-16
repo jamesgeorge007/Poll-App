43500dbd9ca630043f71c80093a3d32919d40586
James George <jamesgeorge998001@gmail.com>
2018-02-16 22:19:45 +0530

    Updated views and UrlsConf.

--- polls/views.py
+++ polls/views.py
@@ -7,3 +7,15 @@ from django.http import HttpResponse
 def index(request):
 
     return HttpResponse('<h1>This is the polls index!</h1>')
+
+def details(request, question_id):
+
+    return HttpResponse('You\'re looking at question %s.' %question_id)
+
+def results(request, question_id):
+
+    return HttpResponse('You\'re looking at the results of question %s' %question_id)
+
+def vote(request, question_id):
+
+    return HttpResponse('You\'re voting on question %s.' %question_id)
